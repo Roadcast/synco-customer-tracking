06bf3f6d3ad5b813141d880554ce95caff5c5017
Digvijay Pandey <digvijay.pandey@roadcast.net>
2022-11-23 23:56:51 +0530

    add old rider icon add, currency symbol by api

--- src/app/order.service.ts
+++ src/app/order.service.ts
@@ -1,7 +1,6 @@
 import {Injectable} from '@angular/core';
 import {Router} from "@angular/router";
 import {HttpBackend, HttpClient} from "@angular/common/http";
-import {log} from "util";
 import {BehaviorSubject} from "rxjs";
 import {environment} from "../environments/environment";
 
@@ -38,10 +37,10 @@ export class OrderService {
         this.order_status = data.order_status
         this.orderPayment = data.order_payment[0]
         this.body_temp = data.rider_avail
-        // const company = await fetch(api_url + 'order/get_redis_company/' + myParam, {
-        //     method: "GET",
-        // });
-        // const companyData = await company.json();
-        // this.currencyCode = companyData.data.currencyCode;
+        const company = await fetch(api_url + 'order/get_redis_company/' + myParam, {
+            method: "GET",
+        });
+        const companyData = await company.json();
+        this.currencyCode = companyData.data.currencyCode;
     }
 }

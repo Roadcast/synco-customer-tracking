cf38580d24c6acb224e9791f4388d7c9a621d02b
Digvijay Pandey <digvijay.pandey@roadcast.net>
2023-01-11 11:39:09 +0530

    commit environment

--- src/environments/environment.uat.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    appVersion: require('../../package.json').version + '-uat',
-
-    production: false,
-    apiUrl: 'https://jfl-api-uat.roadcast.co.in/api/v1/',
-    hereApiKey: '3Xz_d2T_zyHKm1WhBNRAdtmToAQ_HhLmWtMstKeFo34',
-};
